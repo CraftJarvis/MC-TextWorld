2ce483aa1dbcd74a15110c89f9f1a0db6f4bfc46
Zihao Wang <28754930+zhwang4ai@users.noreply.github.com>
2023-05-25 15:20:08 +0800

    sync with guanzhou

---
+++ agent.py
@@ -0,0 +1,20 @@
+class Agent:
+    '''
+    Agent: This is a agent for test the online planner in Minecraft.
+    '''
+    def __init__(self):
+        pass
+
+    def parse_action(self):
+        pass
+
+    def parse_goal(self):
+        pass
+
+class DEPS_Agent(Agent):
+    '''
+    DEPS_Agent: This is a agent for test the online planner in Minecraft.
+    '''
+    def __init__(self):
+        pass
+
--- simulator.py
+++ simulator.py
@@ -1,8 +1,17 @@
 import json
+import os
+import copy
+
+import gym
+from gym import spaces
+from gym.utils import seeding
 
 MAXIMUM_STEP = 100
 
-class Env:
+prefix = os.getcwd()
+goal_lib_path = os.path.join(prefix, 'goal_lib.json')
+
+class Env(gym.Env):
     '''
     Minecraft Simulator: This is a simulator for test the online planner in Minecraft.
     '''
@@ -11,20 +20,31 @@ class Env:
         self.legal_actions = ["mine", "craft", "smelt"]
         self.task_obj = task_obj
 
+        self.goal_lib = self.load_goal_lib()
+
         self.reset()
         
+    def load_goal_lib(self):
+        with open(goal_lib_path, 'r') as f:
+            context = json.load(f)
+        return dict(context)
+
     def reset(self):
         self.state = {
-            'inventory': self.init_inv
+            'inventory': copy.deepcopy(self.init_inv)
         }
         self.reward = 0
         self.done = False
         self.info = {}
         self.curr_step = 0
 
+        return self.state # observation?
+
     # check the action is valid or not
     def check_action(self, action):
         # assert action in ["mine", "craft", "smelt"]
+        if action not in self.goal_lib:
+            raise Exception('Invalid action!')
         pass
 
     def run_action(self, action):
@@ -35,15 +55,61 @@ class Env:
         elif action == "smelt":
             pass
         else:
-            action = no_op()
+            # action = no_op()
+            pass
 
-    def step(self, action):
+    def step(self, action: str):
         '''
         return next state, reward, done, info
         '''
+        # Check action validation
         self.check_action(action)
         self.curr_step += 1
+
+        # Check action preconditions
+        pre_flag = True
+        goal = self.goal_lib[action]
+        for key, val in goal['precondition'].items():
+            if key not in self.state['inventory']:
+                pre_flag = False
+                break
+            if self.state['inventory'][key] < val:
+                pre_flag = False
+                break
+        for key, val in goal['tool'].items():
+            if key not in self.state['inventory']:
+                pre_flag = False
+                break
+            if self.state['inventory'][key] < val:
+                pre_flag = False
+                break
         
+        if not pre_flag:
+            return self.state, 0, self.done, {'success': False}
+
+        # Execute action
+        for key, val in goal['precondition'].items():
+            self.state['inventory'][key] -= val
+            if self.state['inventory'][key] == 0:
+                del self.state['inventory'][key]
+        for key, val in goal['output'].items():
+            if key not in self.state['inventory']:
+                self.state['inventory'][key] = 0
+            self.state['inventory'][key] += val
+
+        # Check if the task is done
+        is_done = True
+        for key, val in self.task_obj.items():
+            if key not in self.state['inventory']:
+                is_done = False
+                break
+            if self.state['inventory'][key] < val:
+                is_done = False
+                break
+        if is_done:
+            self.done = True
+
+        return self.state, 0, self.done, {'success': True}
 
     # def check_item(self, item:str):
     #     if item in self.state['inventory'].keys():
@@ -59,26 +125,6 @@ class Env:
             else:
                 return False
         return True
-
-class Agent:
-    '''
-    Agent: This is a agent for test the online planner in Minecraft.
-    '''
-    def __init__(self):
-        pass
-
-    def parse_action(self):
-        pass
-
-    def parse_goal(self):
-        pass
-
-class DEPS_Agent(Agent):
-    '''
-    DEPS_Agent: This is a agent for test the online planner in Minecraft.
-    '''
-    def __init__(self):
-        pass
     
 if __name__ == '__main__':
     env = Env(
