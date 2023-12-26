801a18851897777eced54d58cbdaac051973b805
Zihao Wang <28754930+zhwang4ai@users.noreply.github.com>
2023-12-26 12:42:20 +0800

    fix mermaid

--- mctextworld/utils.py
+++ mctextworld/utils.py
@@ -1,3 +1,4 @@
+import json
 
 # check if all items in inventory
 def check_dict(inventory:dict, item:dict):
@@ -8,5 +9,33 @@ def check_dict(inventory:dict, item:dict):
         else:
             return False
     return True
+
+def generate_mermaid(input_file: str = 'action_lib.json', output_file: str = 'graph_mermaid.txt'):
+    '''
+    Visit the https://mermaid.live/ page to see the graph
+    '''
+    with open(input_file, 'r') as f:
+        goal_lib = json.load(f)
+
+    f = open(output_file, 'w') 
+    f.write("graph TD\n")
+
+    for key in goal_lib.keys():
+        action = goal_lib[key]
+        output_item = list(action['output'].keys())[0]
+        # print(output_item)
+        precondition = list(action['precondition'].keys())
+        if len(precondition) > 0:
+            # print("&".join(precondition))
+            print(f"  {output_item} --> {'&'.join(precondition)}")
+            f.write(f"  {output_item} --> {'&'.join(precondition)}\n")
+
+        tool = list(action['tool'].keys())
+        if len(tool) > 0:
+            # print("&".join(tool))
+            print(f"  {output_item} --> {'&'.join(tool)}")
+            f.write(f"  {output_item} --> {'&'.join(tool)}\n")
+
+    f.close()
 
 
